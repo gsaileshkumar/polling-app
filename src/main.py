15bd5d248e6f170b88031683670cb17b32cd92cf
Sailesh Kumar <sailesh.kumar@chordx.co>
2020-06-25 01:22:41 +0800

    added jsonify

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,19 +6,20 @@ polls=[]
 
 @app.route('/polls', methods=['GET'])
 def get_all_polls():
-  return polls
+  return jsonify(polls)
 
 @app.route('/poll', methods=['POST'])
 def create_poll():
-  return request.get_json()
+  polls.append(request.get_json())
+  return jsonify(request.get_json())
 
 @app.route('/poll/<id>', methods=['GET'])
 def get_poll(id):
-  return polls[id]
+  return jsonify(polls[id])
 
 @app.route('/vote', methods=['POST'])
 def vote_poll():
-  return request.get_json()
+  return jsonify(request.get_json())
 
 if __name__ == '__main__':
     app.run()
