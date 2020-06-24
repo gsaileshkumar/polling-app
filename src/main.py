32b3906b7a7856d7008756ddaf513c8c3ac33432
Sailesh Kumar <sailesh.kumar@chordx.co>
2020-06-24 22:35:06 +0800

    update polls api

--- src/main.py
+++ src/main.py
@@ -1,22 +1,24 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+polls=[]
+
 @app.route('/polls', methods=['GET'])
 def get_all_polls():
-  return 'get all polls'
+  return polls
 
 @app.route('/poll', methods=['POST'])
 def create_poll():
-  return 'create poll'
+  return request.get_json()
 
 @app.route('/poll/<id>', methods=['GET'])
 def get_poll(id):
-  return 'get poll for id {}'.format(id)
+  return polls[id]
 
 @app.route('/vote', methods=['POST'])
 def vote_poll():
-  return 'vote a poll'
+  return request.get_json()
 
 if __name__ == '__main__':
     app.run()
