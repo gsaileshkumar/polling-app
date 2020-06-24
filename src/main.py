6bffb6d0fc617701bd5df3a513e463d2201d6c8d
Sailesh Kumar <sailesh.kumar@chordx.co>
2020-06-24 22:11:32 +0800

    commit routes

--- src/main.py
+++ src/main.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def get_all_polls():
   return 'get all polls'
 
+@app.route('/poll', methods=['POST'])
+def create_poll():
+  return 'create poll'
+
+@app.route('/poll/<id>', methods=['GET'])
+def get_poll(id):
+  return 'get poll for id {}'.format(id)
+
+@app.route('/vote', methods=['POST'])
+def vote_poll():
+  return 'vote a poll'
+
 if __name__ == '__main__':
     app.run()
